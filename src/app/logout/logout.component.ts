e8fce68c41dc83e6765d6eb01d80b9c0ab2755ff
KatowProject <naufalfaqih443@gmail.com>
2023-03-26 15:08:47 +0700

    partial update

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -8,41 +8,41 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss'],
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent {
 
   constructor(private db: StorageService, private router: Router, public alertController: AlertController) {
   }
 
-  ngOnInit() {
-    const username = this.db.get('username');
-    const password = this.db.get('password');
-    if (!username) {
-      this.router.navigateByUrl('home');
-    }
+  // ngOnInit() {
+  //   const username = this.db.get('username');
+  //   const password = this.db.get('password');
+  //   if (!username) {
+  //     this.router.navigateByUrl('home');
+  //   }
 
-    this.alertController.create({
-      header: 'Alert',
-      subHeader: 'Warning',
-      message: 'Are you sure you want to logout?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: (blah) => {
-            this.router.navigateByUrl('home');
-          }
-        },
-        {
-          text: 'Okay',
-          handler: () => {
-            this.db.remove('username');
-            this.db.remove('password');
-            this.router.navigateByUrl('login');
-          }
-        }
-      ]
-    }).then(alert => alert.present());
-  }
+  //   this.alertController.create({
+  //     header: 'Alert',
+  //     subHeader: 'Warning',
+  //     message: 'Are you sure you want to logout?',
+  //     buttons: [
+  //       {
+  //         text: 'Cancel',
+  //         role: 'cancel',
+  //         cssClass: 'secondary',
+  //         handler: (blah) => {
+  //           this.router.navigateByUrl('home');
+  //         }
+  //       },
+  //       {
+  //         text: 'Okay',
+  //         handler: () => {
+  //           this.db.remove('username');
+  //           this.db.remove('password');
+  //           this.router.navigateByUrl('login');
+  //         }
+  //       }
+  //     ]
+  //   }).then(alert => alert.present());
+  // }
 
 }
